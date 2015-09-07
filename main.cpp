0f25d29b4d203502d42d12a14cc9ec1f7ab5d11b
Babosa <mysunpany@gmail.com>
2015-09-07 19:35:55 +0800

    详细调用说明

--- main.cpp
+++ main.cpp
@@ -16,15 +16,27 @@ Easy_RTSP_Handle fRTSPHandle = 0;
 /* RTSPClient数据回调 */
 int Easy_APICALL __RTSPClientCallBack( int _chid, int *_chPtr, int _frameType, char *_pBuf, RTSP_FRAME_INFO* _frameInfo)
 {
-	if (_frameType == EASY_SDK_VIDEO_FRAME_FLAG)
+	if (_frameType == EASY_SDK_VIDEO_FRAME_FLAG)//回调视频数据
 	{
-		printf("Get %s Video Len:%d \ttimestamp:%u.%u\n",_frameInfo->type==EASY_SDK_VIDEO_FRAME_I?"I":"P", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		if (_frameInfo->codec == EASY_SDK_VIDEO_CODEC_H264)
+			printf("Get %s H264 Len:%d \ttimestamp:%u.%u\n",_frameInfo->type==EASY_SDK_VIDEO_FRAME_I?"I":"P", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		else if (_frameInfo->codec == EASY_SDK_VIDEO_CODEC_MJPEG)
+			printf("Get MJPEG Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		else if (_frameInfo->codec == EASY_SDK_VIDEO_CODEC_MPEG4)
+			printf("Get MPEG4 Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
 	}
-	else if (_frameType == EASY_SDK_AUDIO_FRAME_FLAG)
+	else if (_frameType == EASY_SDK_AUDIO_FRAME_FLAG)//回调音频数据
 	{
-		printf("Get %u Audio Len:%d \ttimestamp:%u.%u\n", _frameInfo->codec, _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		if (_frameInfo->codec == EASY_SDK_AUDIO_CODEC_AAC)
+			printf("Get AAC Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		else if (_frameInfo->codec == EASY_SDK_AUDIO_CODEC_G711A)
+			printf("Get PCMA Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		else if (_frameInfo->codec == EASY_SDK_AUDIO_CODEC_G711U)
+			printf("Get PCMU Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
+		else if (_frameInfo->codec == EASY_SDK_AUDIO_CODEC_G726)
+			printf("Get G726 Len:%d \ttimestamp:%u.%u\n", _frameInfo->length, _frameInfo->timestamp_sec, _frameInfo->timestamp_usec);
 	}
-	else if (_frameType == EASY_SDK_EVENT_FRAME_FLAG)
+	else if (_frameType == EASY_SDK_EVENT_FRAME_FLAG)//回调连接状态事件
 	{
 		if (NULL == _pBuf && NULL == _frameInfo)
 		{
@@ -35,6 +47,17 @@ int Easy_APICALL __RTSPClientCallBack( int _chid, int *_chPtr, int _frameType, c
 			printf("Lose Packet:%s ...\n", RTSPURL);
 		}
 	}
+	else if (_frameType == EASY_SDK_MEDIA_INFO_FLAG)//回调出媒体信息
+	{
+		if(_pBuf != NULL)
+		{
+			EASY_MEDIA_INFO_T mediainfo;
+			memset(&mediainfo, 0x00, sizeof(EASY_MEDIA_INFO_T));
+			memcpy(&mediainfo, _pBuf, sizeof(EASY_MEDIA_INFO_T));
+			printf("RTSP DESCRIBE Get Media Info: video:%u fps:%u audio:%u channel:%u sampleRate:%u \n", 
+				mediainfo.u32VideoCodec, mediainfo.u32VideoFps, mediainfo.u32AudioCodec, mediainfo.u32AudioChannel, mediainfo.u32AudioSamplerate);
+		}
+	}
 	return 0;
 }
 
@@ -43,11 +66,13 @@ int main()
 	//创建RTSPSource
 	EasyRTSP_Init(&fRTSPHandle);
 
+	// 可以根据fRTSPHanlde判断，也可以根据EasyRTSP_Init是否返回0判断
 	if (NULL == fRTSPHandle) return 0;
 	
 	// 设置数据回调处理
 	EasyRTSP_SetCallback(fRTSPHandle, __RTSPClientCallBack);
 
+	// 设置接收音频、视频数据
 	unsigned int mediaType = EASY_SDK_VIDEO_FRAME_FLAG | EASY_SDK_AUDIO_FRAME_FLAG;
 
 	// 打开RTSP流
@@ -55,7 +80,10 @@ int main()
 
 	getchar();
    
+	// 关闭RTSPClient
 	EasyRTSP_CloseStream(fRTSPHandle);
+
+	// 释放RTSPHandle
 	EasyRTSP_Deinit(&fRTSPHandle);
 	fRTSPHandle = NULL;
 
